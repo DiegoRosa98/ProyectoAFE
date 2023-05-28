bb9055adbebf476c8ab421226b85a3868bd27594
Israel Mendez <igonzalez@hightech-corp.com>
2023-05-27 23:00:12 -0600

    create models

---
+++ app/Models/usuarios.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class usuarios extends Model
+{
+    use HasFactory;
+    // table name
+    protected $table = "usuarios";
+
+    /**
+     * The attributes that are mass assignable.
+     * @var array<int, string>
+     */
+    protected $fillable = ['correo', 'username', 'idRol', 'estado', 'created_at', 'updated_at'];
+    /**
+     * The attributes that should be hidden for serialization.
+     * @var array<int, string>
+     */
+    protected $hidden = ['id', 'clave'];
+
+    /**
+     * Method for retrieving all users' list
+     */
+    public function getUsers() {
+        return usuarios::all();
+    }
+
+    /**
+     * Method for retrieving an especific user
+     */
+    public function getUserById($id) {
+        return usuarios::find($id);
+    }
+
+}
